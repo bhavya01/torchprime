3581a367efa730d64d3724f4b37818d376607027
Bhavya <bbahl@google.com>
2024-09-27 00:37:10 +0000

    Fix code

--- trainer.py
+++ trainer.py
@@ -129,7 +129,7 @@ class Trainer:
         self,
         model: nn.Module,
         args: MoreTrainingArguments,
-        train_dataset: Optional[Union[Dataset, IterableDataset]],
+        train_dataset: Optional[Union[Dataset, IterableDataset]]
     ):
         self.args = args
         self.device = xm.xla_device()
@@ -321,4 +321,52 @@ def main():
     logger.info(
         f"Training new model from scratch - Total size={n_params} params")
 
-    # Set the model d
+    # Set the model dtype to bfloat16
+    model = model.to(torch.bfloat16)
+    # Downloading and loading a dataset from the hub.
+    data = load_dataset(
+        data_args.dataset_name,
+        data_args.dataset_config_name,
+        cache_dir=model_args.cache_dir,
+    )["train"]
+    column_names = list(data.features)
+    data = data.map(lambda samples: tokenizer(samples["text"]),
+                    batched=True,
+                    remove_columns=column_names)
+    # Taken from run_clm.py. It's important to group texts evenly to avoid recompilations in TPU.
+    block_size = data_args.block_size
+    def group_texts(examples):
+        from itertools import chain
+        # Concatenate all texts.
+        concatenated_examples = {
+            k: list(chain(*examples[k]))
+            for k in examples.keys()
+        }
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
+        # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
+        total_length = (total_length // block_size) * block_size
+        # Split by chunks of max_len.
+        result = {
+            k:
+            [t[i:i + block_size] for i in range(0, total_length, block_size)]
+            for k, t in concatenated_examples.items()
+        }
+        result["labels"] = result["input_ids"].copy()
+        return result
+
+    logger.info(f"Grouping data into chunks of {block_size}")
+    data = data.map(group_texts, batched=True)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=data,
+    )
+    trainer.train_loop()
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    main()
